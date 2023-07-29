6563e72fbf71d0c830c43fa9bac1bd23f14ccb26
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-07-29 14:38:33 +0500

    working on og image

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Dayim Marketing',
   description: 'Building Your Visions. Creating Reality',
+  images: [
+    {
+      url: '/images/dsa/dayim_sub_logo.PNG',
+      width: 800,
+      height: 600,
+    }
+  ],
 }
 
 export default function RootLayout({ children }) {
