9c0b5766101494895247aa6d078c86bdcd61f5dc
Your Name~ <you@example.com>
2024-03-15 02:22:53 +0500

    ...

--- src/app/dayim-signature-apartments/components/Tabs/HomeTab.js
+++ src/app/dayim-signature-apartments/components/Tabs/HomeTab.js
@@ -1,7 +1,7 @@
 "use client";
-import { Fragment } from 'react'
-import { Tab } from '@headlessui/react'
-import HomeTabItem from './HomeTabItem';
+import { Fragment } from "react";
+import { Tab } from "@headlessui/react";
+import HomeTabItem from "./HomeTabItem";
 
 export default function HomeTab() {
   return (
@@ -11,7 +11,11 @@ export default function HomeTab() {
           {({ selected }) => (
             <button
               className={`
-                border-b-2 ${selected ? 'text-second  border-second ' : 'bg-white text-black border-gray'}
+                border-b-2 ${
+                  selected
+                    ? "text-second  border-second "
+                    : "bg-white text-black border-gray"
+                }
               `}
             >
               Lower Ground Floor
@@ -22,7 +26,11 @@ export default function HomeTab() {
           {({ selected }) => (
             <button
               className={`
-                border-b-2 ${selected ? 'text-second  border-second ' : 'bg-white text-black border-gray'}
+                border-b-2 ${
+                  selected
+                    ? "text-second  border-second "
+                    : "bg-white text-black border-gray"
+                }
               `}
             >
               Ground Floor
@@ -33,7 +41,11 @@ export default function HomeTab() {
           {({ selected }) => (
             <button
               className={`
-                border-b-2 ${selected ? 'text-second  border-second ' : 'bg-white text-black border-gray'}
+                border-b-2 ${
+                  selected
+                    ? "text-second  border-second "
+                    : "bg-white text-black border-gray"
+                }
               `}
             >
               1st Floor
@@ -44,28 +56,75 @@ export default function HomeTab() {
           {({ selected }) => (
             <button
               className={`
-                border-b-2 ${selected ? 'text-second  border-second ' : 'bg-white text-black border-gray'}
+                border-b-2 ${
+                  selected
+                    ? "text-second  border-second "
+                    : "bg-white text-black border-gray"
+                }
               `}
             >
-              2nd - 6th Floor
+              2nd - 4th Floor
             </button>
           )}
-        </Tab>        
+        </Tab>
+        <Tab as={Fragment} className="outline-none h-full mt-2 w-[150px] pb-5 ">
+          {({ selected }) => (
+            <button
+              className={`
+                border-b-2 ${
+                  selected
+                    ? "text-second  border-second "
+                    : "bg-white text-black border-gray"
+                }
+              `}
+            >
+              5th - 6th Floor
+            </button>
+          )}
+        </Tab>
       </Tab.List>
       <Tab.Panels>
         <Tab.Panel>
-          <HomeTabItem image="/images/dsa/tabs/lower-ground.jpg" size="895" availableUnits="#" freeUnits="2 Shops/Offices Available" />
+          <HomeTabItem
+            image="/images/dsa/tabs/LowerGround.png"
+            size="895"
+            availableUnits="#"
+            freeUnits="2 Shops/Offices Available"
+          />
         </Tab.Panel>
         <Tab.Panel>
-          <HomeTabItem image="/images/dsa/tabs/ground.jpg" size="832" availableUnits="#" freeUnits="1 Shop/Office Available" />
+          <HomeTabItem
+            image="/images/dsa/tabs/GroundFloor.png"
+            size="832"
+            availableUnits="#"
+            freeUnits="1 Shop/Office Available"
+          />
         </Tab.Panel>
         <Tab.Panel>
-          <HomeTabItem image="/images/dsa/tabs/1st.jpg" size="895" availableUnits="#" freeUnits="2 Shops/Offices Available" />
+          <HomeTabItem
+            image="/images/dsa/tabs/FirstFloor.png"
+            size="895"
+            availableUnits="#"
+            freeUnits="2 Shops/Offices Available"
+          />
         </Tab.Panel>
         <Tab.Panel>
-          <HomeTabItem image="/images/dsa/tabs/2nd-6th.jpg" size="353" availableUnits="#" freeUnits="15 Apartments Available" />
-        </Tab.Panel>        
+          <HomeTabItem
+            image="/images/dsa/tabs/2nd-4th-Floor.png"
+            size="353"
+            availableUnits="#"
+            freeUnits="15 Apartments Available"
+          />
+        </Tab.Panel>
+        <Tab.Panel>
+          <HomeTabItem
+            image="/images/dsa/tabs/5th-6th-Floor.png"
+            size="353"
+            availableUnits="#"
+            freeUnits="15 Apartments Available"
+          />
+        </Tab.Panel>
       </Tab.Panels>
     </Tab.Group>
-  )
+  );
 }
--- src/app/dmComponents/footer/Footer1.js
+++ src/app/dmComponents/footer/Footer1.js
@@ -1,84 +1,143 @@
 "use client";
 
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 import { FaFacebookF, FaInstagram, FaLinkedin } from "react-icons/fa";
 // import HomeSlider5 from '../sliders/FooterSlider1';
-import FooterSlider1 from './FooterSlider1';
-import FooterSlider2 from './FooterSlider2';
+import FooterSlider1 from "./FooterSlider1";
+import FooterSlider2 from "./FooterSlider2";
 import { AiFillYoutube } from "react-icons/ai";
 
-
 function Footer1() {
-    const b_t_animation = {
-        hide: {
-            opacity: 0,
-            y: 300,
-        },
-        show: {
-            opacity: 1,
-            y: 0,
-            transition: {
-                duration: 1,
-            },
-        },
-    };
+  const b_t_animation = {
+    hide: {
+      opacity: 0,
+      y: 300,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
 
-    return (
-    <div className='bg-black text-white min-h-[500px]
-                    md:pt-20'>
-        <motion.section 
-            initial="hide"
-            whileInView="show"
-            variants={b_t_animation}
-            className='flex flex-col justify-center gap-10 border-b border-gray py-20 p-5
-                            lg:flex-row lg:gap-0 md:p-20'>
-            <h3 className='w-[100%] text-6xl font-bold 
-                            lg:w-[50%] lg:text-center text-second'>Developer</h3>
-            <div className='w-[100%] text-gray  text-lg text-justify
-                            lg:w-[50%] md:pr-[200px]'>
-                <p><span className='text-second'>DAYIM MARKETING</span> is a team of specialists - engineers, architects, project managers, technicians and other experts, all of whom share a common goal: a positive and responsible development of the city that takes into account the wellbeing of its people and the environment.</p>
-                <div className='flex flex-col gap-5 items-center mt-10
-                                md:flex-row md:gap-20'>
-                    {/* <Link href="https://globalbusinessconcern.com/" target='_blank' className='text-white underline hover:text-second transition'>globalbussinessconcern.com</Link> */}
-                    <div className=' flex gap-5'>
-                        <Link href="https://www.facebook.com/dayimmarketing/" target='_blank'>                            
-                            {/* <AiFillYoutube className=' rounded-full p-3 bg-gray text-white cursor-pointer hover:bg-second' size={40} /> */}                
-                            <img src="/images/dsa/icons/facebook.svg" alt="" className='w-[30px] h-[30px] hover:scale-110' />
-                        </Link>
-                        <Link href="https://www.instagram.com/Dayimmarketing/" target='_blank'>
-                            <img src="/images/dsa/icons/instagram.svg" alt="" className='w-[30px] h-[30px] hover:scale-110' />
-                        </Link>                        
-                        <Link href="https://www.youtube.com/c/DayimMarketing" target='_blank'>
-                            <img src="/images/dsa/icons/youtube.svg" alt="" className='w-[30px] h-[30px] hover:scale-110' />
-                        </Link>                  
-                        <Link href="https://www.linkedin.com/company/dayim-marketing" target='_blank'>
-                            <img src="/images/dsa/icons/linkedin.svg" alt="" className='w-[30px] h-[30px] hover:scale-110' />
-                        </Link>                  
-                        <Link href="https://www.twitter.com/Dayimmarketing" target='_blank'>
-                            <img src="/images/dsa/icons/twitter.svg" alt="" className='w-[30px] h-[30px] hover:scale-110' />
-                        </Link>                  
-                        <Link href="https://api.whatsapp.com/send?phone=923085111176" target='_blank'>
-                            <img src="/images/dsa/icons/whatsapp.png" alt="" className='w-[30px] h-[30px] hover:scale-110' />
-                        </Link>                  
-                    </div>
-                </div>
+  return (
+    <div
+      className="bg-black text-white min-h-[500px]
+                    md:pt-20"
+    >
+      <motion.section
+        initial="hide"
+        whileInView="show"
+        variants={b_t_animation}
+        className="flex flex-col justify-center gap-10 border-b border-gray py-20 p-5
+                            lg:flex-row lg:gap-0 md:p-20"
+      >
+        <h3
+          className="w-[100%] text-6xl font-bold 
+                            lg:w-[50%] lg:text-center text-second"
+        >
+          Developer
+        </h3>
+        <div
+          className="w-[100%] text-gray  text-lg text-justify
+                            lg:w-[50%] md:pr-[200px]"
+        >
+          <p>
+            <span className="text-second">DAYIM DEVELOPER</span> is a team of
+            specialists - engineers, architects, project managers, technicians
+            and other experts, all of whom share a common goal: a positive and
+            responsible development of the city that takes into account the
+            wellbeing of its people and the environment.
+          </p>
+          <div
+            className="flex flex-col gap-5 items-center mt-10
+                                md:flex-row md:gap-20"
+          >
+            {/* <Link href="https://globalbusinessconcern.com/" target='_blank' className='text-white underline hover:text-second transition'>globalbussinessconcern.com</Link> */}
+            <div className=" flex gap-5">
+              <Link
+                href="https://www.facebook.com/dayimmarketing/"
+                target="_blank"
+              >
+                {/* <AiFillYoutube className=' rounded-full p-3 bg-gray text-white cursor-pointer hover:bg-second' size={40} /> */}
+                <img
+                  src="/images/dsa/icons/facebook.svg"
+                  alt=""
+                  className="w-[30px] h-[30px] hover:scale-110"
+                />
+              </Link>
+              <Link
+                href="https://www.instagram.com/Dayimmarketing/"
+                target="_blank"
+              >
+                <img
+                  src="/images/dsa/icons/instagram.svg"
+                  alt=""
+                  className="w-[30px] h-[30px] hover:scale-110"
+                />
+              </Link>
+              <Link
+                href="https://www.youtube.com/c/DayimMarketing"
+                target="_blank"
+              >
+                <img
+                  src="/images/dsa/icons/youtube.svg"
+                  alt=""
+                  className="w-[30px] h-[30px] hover:scale-110"
+                />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/company/dayim-marketing"
+                target="_blank"
+              >
+                <img
+                  src="/images/dsa/icons/linkedin.svg"
+                  alt=""
+                  className="w-[30px] h-[30px] hover:scale-110"
+                />
+              </Link>
+              <Link
+                href="https://www.twitter.com/Dayimmarketing"
+                target="_blank"
+              >
+                <img
+                  src="/images/dsa/icons/twitter.svg"
+                  alt=""
+                  className="w-[30px] h-[30px] hover:scale-110"
+                />
+              </Link>
+              <Link
+                href="https://api.whatsapp.com/send?phone=923085111176"
+                target="_blank"
+              >
+                <img
+                  src="/images/dsa/icons/whatsapp.png"
+                  alt=""
+                  className="w-[30px] h-[30px] hover:scale-110"
+                />
+              </Link>
             </div>
-        </motion.section>
+          </div>
+        </div>
+      </motion.section>
 
-        <motion.section 
-            initial="hide"
-            whileInView="show"
-            variants={b_t_animation}
-            className=' p-5
-                        lg:px-20 md:mt-10'>
-            <FooterSlider2 />
-        </motion.section>
+      <motion.section
+        initial="hide"
+        whileInView="show"
+        variants={b_t_animation}
+        className=" p-5
+                        lg:px-20 md:mt-10"
+      >
+        <FooterSlider2 />
+      </motion.section>
 
-        {/* <motion.section 
+      {/* <motion.section 
             initial="hide"
             whileInView="show"
             variants={b_t_animation}
@@ -144,7 +203,7 @@ function Footer1() {
              
         </motion.section> */}
     </div>
-  )
+  );
 }
 
-export default Footer1
+export default Footer1;
